81eb8e576d5f63bc7db4072838a51eea1a935b1f
Cameron Drummond <48034712+CamCommand@users.noreply.github.com>
2021-08-12 12:51:02 -0400

    Update binaryloop.py

--- binaryloop.py
+++ binaryloop.py
@@ -5,12 +5,12 @@ a = 1
 from time import sleep
 import sys
 while a > 0:
-	for line in lines:          # for each line of text
-		for c in line:          # for each charin each line
-			print(c, end='')    # print a single character, and keep the cursor there
-			sys.stdout.flush()  # flush the buffer
-			sleep(0.001)         # wait a fraction of a second
-		print('')               # line break 
+	for line in lines:               			# for each line of text
+		for c in line:         				# for each charin each line
+			print(c, end='')    			# print a single character, and keep the cursor there
+			sys.stdout.flush()  			# flush the buffer
+			sleep(0.001)         			# wait a fraction of a second
+		print('')               			# line break 
 								# right click cmd 
 								# select properties
 								# go to colors and select Screen Text to Green 
